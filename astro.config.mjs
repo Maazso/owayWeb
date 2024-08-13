7874d1eb1743ad1167aad850e489be96f567fead
roberto perez <roberto@YOLO.local>
2024-08-13 14:47:03 +0400

    Up config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://oway.info",
   integrations: [tailwind(), mdx(), sitemap()],
+  output: 'server',
+  adapter: awsAmplify(),
 });
