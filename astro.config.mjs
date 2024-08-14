dde4b42bd21d68634523047b2c4fbae9525eb93b
roberto perez <roberto@YOLO.local>
2024-08-14 14:33:32 +0400

    update with Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import awsAmplify from 'astro-aws-amplify';
+import vercel from '@astrojs/vercel/serverless';
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap()],
   output: 'server',
-  adapter: awsAmplify(),
+  adapter: deno(),
 });
